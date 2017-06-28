2e5596d3f014f380f7bbcf2eb1f49f95dfbfd874
David <dmiller@pilith.com>
2017-06-28 17:19:15 -0500

    changed back to 'default' database. added a component in to test'

--- randomrestaurant/settings.py
+++ randomrestaurant/settings.py
@@ -87,10 +87,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'removed': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'removed_comp.sqlite3'),
-    }
+    #'removed': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'removed_comp.sqlite3'),
+    #}
 }
 
 
--- removed/admin.py
+++ removed/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from removed.models import component
+admin.site.register(component)
