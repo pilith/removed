c4f8863f77154da5dfa632125c33eb62798d7298
David <dmiller@pilith.com>
2017-06-28 15:27:16 -0500

    made first revision of board model for removed components

--- removed/models.py
+++ removed/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class component(models.Model):
+    board_handling = (
+        ('R', 'RMA'),
+        ('P', 'Production'),
+    )
+    entry_date = models.DateField()
+    board = model.CharField(max_length=30)
+    part_num = model.PositiveIntegerField()
+    serial_num = model.PositiveIntegerField()
+    handling = model.CharField(max_length=1, choices=board_handling)
+    notes = model.CharField(max_length=100, blank=True)
+    user_name = model.CharField(max_length = 30)
