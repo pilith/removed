2475d9db2ae24f2b29e79da8b62120f4b5738f1c
jzarnstorff <jakez03@hotmail.com>
2017-07-01 15:32:58 -0500

    initial commit of removed component webpage layout

--- randomrestaurant/urls.py
+++ randomrestaurant/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^removed/', include('removed.urls', namespace='removed')),
 ]
 
--- removed/views.py
+++ removed/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def removed(request):
+    return render(request, 'removed/removed.html')
