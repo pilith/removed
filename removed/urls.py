b30458cc190e2f47e6563e57691ced03b5a65030
jzarnstorff <jakez03@hotmail.com>
2017-07-02 03:52:55 -0500

    fixed overall structure of app

--- removed/urls.py
+++ removed/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.removed, name='removed'),
+    url(r'^$', views.index, name='index'),
+    url(r'^removed_components$', views.removed, name='removed'),
+    url(r'^fixed_list$', views.fixed, name='fixed'),
 ]
--- removed/views.py
+++ removed/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request, 'removed/index.html')
+
 def removed(request):
     return render(request, 'removed/removed.html')
+
+def fixed(request):
+    return render(request, 'removed/fixed.html')
