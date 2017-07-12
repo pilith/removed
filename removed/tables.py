7e07d4510e79e43841393601d44dadd0017cff76
David <dmiller@pilith.com>
2017-07-12 15:16:55 -0500

    added edit column to tables, sees the id properly for editing

--- removed/tables.py
+++ removed/tables.py
@@ -5,11 +5,13 @@ from .models import component
 from .models import fixed_board
 
 class ComponentTable(tables.Table):
+    edit_entries = tables.TemplateColumn('<a href="{{record.id}}">Edit</a>')
     class Meta:
         model = component
         attrs = {'class': 'paleblue'}
 
 class boardTable(tables.Table):
+    edit_entries = tables.TemplateColumn('<a href="{{record.id}}">Edit</a>')
     class Meta:
         model = fixed_board
         attrs = {'class': 'paleblue'}
